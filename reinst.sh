78346b49ab5fe739067f241ee6a63a8fd0e9bed3
Matt Parsons <parsonsmatt@gmail.com>
2015-01-31 19:38:45 -0500

    change install to go to release of 0.3.0 branch

--- reinst.sh
+++ reinst.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 phonegap plugin remove com.pylonproducts.wifiwizard
-phonegap plugin add http://github.com/parsonsmatt/WifiWizard.git
+phonegap plugin add http://github.com/parsonsmatt/WifiWizard.git#release/0.3.0
